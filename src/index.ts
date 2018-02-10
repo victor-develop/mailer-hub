6e91d2a15159099b82d72fcf7242395f42d767b7
Victor Z <victor.develop@outlook.com>
2018-02-11 00:58:20 +0800

    add: index.ts, expose things for external use

---
+++ src/index.ts
@@ -0,0 +1,5 @@
+import { mailerHubBuilder } from './mailer-hub';
+import * as Interfaces from './interfaces';
+import * as ErrorTypes from './errors';
+
+export { mailerHubBuilder, Interfaces, ErrorTypes };
