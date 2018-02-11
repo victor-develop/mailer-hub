8c9c376b1516ff685d6a9e3bede0c2b44d1adc94
Victor Z <victor.develop@outlook.com>
2018-02-12 04:26:45 +0800

    add : example secret file for testing

---
+++ src/tests/data/secret/mailgun.secret.exmaple.ts
@@ -0,0 +1,9 @@
+import { IMailGunConfig } from '../../../interfaces';
+
+const config: IMailGunConfig = {
+  apiKey: 'key-xxxxxxxxxxxxxxxxxxxxx',
+  domain: 'sandboxxxxxxxxxxxxxxxxxxxxxx.mailgun.org',
+  senderAcc: 'postmaster',
+};
+
+export default config;
---
+++ src/tests/data/secret/sendgrid.secret.example.ts
@@ -0,0 +1,9 @@
+import { ISendGridConfig } from '../../../interfaces';
+
+const config: ISendGridConfig = {
+  apiKey: '',
+  domain: 'example.org',
+  senderAcc: 'postmaster',
+};
+
+export default config;
