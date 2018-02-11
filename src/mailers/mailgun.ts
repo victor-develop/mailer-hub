507a3adceaea3715125b51c29eb614dccfc80896
Victor Z <victor.develop@outlook.com>
2018-02-11 13:04:06 +0800

    add: test for mailgun (not ready)

--- src/mailers/mailgun.ts
+++ src/mailers/mailgun.ts
@@ -1,4 +1,4 @@
-import { IMailer, IHubMail, IMailGunConfig } from '../interfaces';
+import { IMailer, IHubMail, IMailGunConfig, sendMail } from '../interfaces';
 import * as MailGun from 'mailgun-js';
 
 export function build(config: IMailGunConfig): IMailer {
@@ -7,26 +7,30 @@ export function build(config: IMailGunConfig): IMailer {
 
   const sender = `${config.senderAcc}@${config.domain}`;
 
-  const mailer: IMailer = {
-    send: (mail: IHubMail) => {
-      const data = {
-        from: sender,
-        to: mail.to,
-        cc: mail.cc,
-        bcc: mail.bcc,
-        subject: mail.subject,
-        text: mail.text,
-      };
-      return new Promise((resolve, reject) => {
-        (<any>mailGun).messages().send(data, (err: any, body: any) => {
-          if (err) {
-            // TODO: log error
-            reject(err);
-          }
-          resolve(body);
-        });
+  const mailer = new MailGunMailer((mail: IHubMail) => {
+    const data = {
+      from: sender,
+      to: mail.to,
+      cc: mail.cc,
+      bcc: mail.bcc,
+      subject: mail.subject,
+      text: mail.text,
+    };
+    return new Promise((resolve, reject) => {
+      (<any>mailGun).messages().send(data, (err: any, body: any) => {
+        if (err) {
+          // TODO: log error
+          reject(err);
+        }
+        resolve(body);
       });
-    },
-  };
+    });
+  });
   return mailer;
+}
+
+export class MailGunMailer implements IMailer {
+  constructor(public readonly send: sendMail) {
+
+  }
 }
---
+++ src/tests/mailers/mailgun.spec.ts
@@ -0,0 +1,5 @@
+import { MailGunMailer } from '../../mailers/mailgun';
+
+describe(MailGunMailer.name, () => {
+  it('can send email with MailGun service');
+});
